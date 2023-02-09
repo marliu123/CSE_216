23df753014150ade58d44f28445949be266d0a13
Marcus Liu <marcus.liu@stonybrook.edu>
2023-02-09 15:03:04 -0500

    edit

--- rec2.ml
+++ rec2.ml
@@ -7,3 +7,17 @@ let tripleFloat x = 3.0*.x;;
 let thrice f x = f(f(f(x)));;
 let composition f g x = f(g(x));;
 let div x y = x/y;;
+let triple3 = thrice tripleFloat;;
+
+let rec repeat f n x = match n with 
+| 0 -> x
+| _ -> repeat f (n - 1) (f x);;
+
+
+let f list =
+let rec aux acc = function
+  | [] -> acc
+  | h::t -> aux (h::acc) t in aux [] list;;
+
+
+
