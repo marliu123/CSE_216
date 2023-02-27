c59cbd441e048cd03957fee2368f8fb3b229d005
Marcus Liu <marcus.liu@stonybrook.edu>
2023-02-26 19:41:50 -0500

    edit

--- HW1.ml
+++ HW1.ml
@@ -10,14 +10,32 @@ let rec float_pow x y =
   | _ -> x *. float_pow x ( y - 1 );;
 
 (*Q2*)
-
 let rec compress lst = 
   match lst with 
   | [] -> []
   | [x] -> [x]
-  | x ::(y :: xs) -> 
+  | x :: y :: xs -> 
     if x = y then compress (y :: xs) 
     else x :: compress (y :: xs);;
    
 (*Q3*)
+let rec remove_if lst predicate = 
+  match lst with 
+  | [] -> []
+  | x :: xs -> if predicate x then remove_if xs predicate
+  else x :: remove_if xs predicate;;
 
+(*Q4*)
+let rec slice lst i j = 
+  let rec sublist lst i j k list = 
+    match lst with 
+    | [] -> list
+    | x :: xs -> if i > k then sublist xs i j (k + 1) list 
+    else if j <= k then list
+    else sublist xs i j (k + 1) (list@[x])
+  in sublist lst i j 0 [];;
+
+(*Q5*)
+
+
+(*Q6*)
